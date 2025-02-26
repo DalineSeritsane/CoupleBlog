35e660b9e411a9a3bda9291a7718512059450d64
DalineSeritsane <dalinelerato@gmail.com>
2025-02-26 12:05:37 +0200

    testing

--- backendblog/index.js
+++ backendblog/index.js
@@ -1,9 +1,6 @@
-
-
 const express = require('express');
-
 const bodyParser = require('body-parser');
-
+const fs = require('fs');
 const cors = require('cors');
 
 const userRoutes = require('./backendBlog/routes/users');
@@ -14,18 +11,26 @@ const path = require('path');
 
 require('dotenv').config();
 
-
-
 const app = express();
-
 const PORT = process.env.PORT || 5000; // Fallback if .env is missing
 
 
 
 app.use(cors());
 
+// Middleware
 app.use(bodyParser.json());
 
+// helper function to read JSON file 
+const readBlogs = () => {
+  const blogsData = fs.readFileSync('blogs.json');
+  return JSON.parse(blogsData);
+};
+
+//function to write JSON file
+const writeBlogs = (data) => {
+  const postsData = fs.readFileSync('blogs.json', JSON.stringify(data, null,2));
+};
 
 
 // Serve static files from the uploads folder
